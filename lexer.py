fceeb8f2c96f471eef5d28539cfd8481b7f189ad
Klaus Rollmann <ra146810@beach.lab.ic.unicamp.br>
2017-03-08 17:06:46 -0300

    Adicionando lexer

---
+++ lexer.py
@@ -0,0 +1,179 @@
+import ply.lex as lex
+import sys
+
+class MyLexer(object):
+
+    # Dict of reserved words
+    reserved = {
+            'array' : 'ARRAY', 
+            'by' : 'BY', 
+            'chars' : 'CHARS', 
+            'dcl' : 'DCL', 
+            'do' : 'DO', 
+            'down' : 'DOWN', 
+            'else' : 'ELSE', 
+            'elsif' : 'ELSIF', 
+            'end' : 'END', 
+            'exit' : 'EXIT', 
+            'fi' : 'FI',
+            'for' : 'FOR', 
+            'if' : 'IF', 
+            'in' : 'IN', 
+            'loc' : 'LOC', 
+            'od' : 'OD', 
+            'print' : 'PRINT',
+            'proc' : 'PROC', 
+            'ref' : 'REF', 
+            'result' : 'RESULT', 
+            'return' : 'RETURN', 
+            'returns' : 'RETURNS',
+            'syn' : 'SYN', 
+            'then' : 'THEN', 
+            'to' : 'TO', 
+            'type' : 'TYPE', 
+            'while' : 'WHILE',
+            'abs' : 'ABS',
+            'asc' : 'ASC',
+            'bool' : 'BOOL',
+            'char' : 'CHAR',
+            'false' : 'FALSE',
+            'int' : 'INT',
+            'length' : 'LENGTH',
+            'lower' : 'LOWER',
+            'null' : 'NULL',
+            'num' : 'NUM',
+            'print' : 'PRINT',
+            'read' : 'READ',
+            'true' : 'TRUE',
+            'upper' : 'UPPER'
+    }
+
+
+    # List of token names.   This is always required
+    tokens = [
+            'ID',
+            'ICONST',
+            'CCONST',
+            'SCONST',
+            'PLUS', 
+            'MINUS',
+            'TIMES',
+            'DIVIDE',
+            'ASSIGN',
+            'SEMI',
+            'ARROW',
+            'LPAREN',
+            'RPAREN',
+            'LBRACKET',
+            'RBRACKET',
+            'AND',
+            'OR',
+            'EQ',
+            'NE',
+            'GT',
+            'GEQ',
+            'LT',
+            'LEQ',
+            'COLON',
+            'COMMA',
+            'NOT'
+            ] + list(reserved.values())
+
+    # Regular expression rules for simple tokens
+    t_PLUS = r'\+'
+    t_MINUS = r'-'
+    t_TIMES = r'\*'
+    t_DIVIDE = r'/'
+    t_ASSIGN = r'='
+    t_SEMI = r';'
+    t_ARROW = r'->'
+    t_LPAREN  = r'\('
+    t_RPAREN  = r'\)'
+    t_LBRACKET = r'\['
+    t_RBRACKET = r'\]'
+    t_AND = r'&&'
+    t_OR = r'\|\|'
+    t_EQ = r'=='
+    t_NE = r'!='
+    t_GT = r'>'
+    t_GEQ = r'>='
+    t_LT = r'<'
+    t_LEQ = r'<='
+    t_COLON = r':'
+    t_COMMA = r','
+    t_NOT = r'!'
+
+    # A regular expression rule with some action code
+    # Note addition of self parameter since we're in a class
+    def t_ICONST(self,t):
+        r'\d+'
+        t.value = int(t.value)    
+        return t
+
+    def t_ID(self,t):
+        r'[A-Za-z_][\dA-Za-z_]*'
+        t.type = MyLexer.reserved.get(t.value, 'ID')
+        return t
+
+    def t_SCOMMENT(self, t):
+        r'\/\/.*\n'
+        pass
+
+    def t_COMMENT(self, t):
+        r'\/\*.*\*\/'
+        pass
+
+    def t_SCONST(self, t):
+        r'\".*\"'
+        return t
+
+    # Define a rule so we can track line numbers
+    def t_newline(self,t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+
+    # A string containing ignored characters (spaces and tabs)
+    t_ignore  = ' \t'
+
+    # Error handling rule
+    def t_error(self,t):
+        print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
+
+    def t_commenterror(self,t):
+        r'\/\*.*'
+        print ('{}: Unterminated comment'.format(t.lexer.lineno))
+        pass
+
+    def t_stringerror(self,t):
+        r'\".*\n'
+        print ('{}: Unterminated string'.format(t.lexer.lineno))
+        t.lexer.skip(1)
+
+    def t_badescape(self,t):
+        r'\a|\r|\f|\v|\\|\'|\"|\?|\b'
+        print ("{}: Bad string escape code '\\..'".format(t.lexer.lineno))
+        t.lexer.skip(1)
+
+    # Build the lexer
+    def build(self,**kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
+
+    # Test it output
+    def test(self,data):
+        self.lexer.input(data)
+        while True:
+            tok = self.lexer.token()
+            if not tok: 
+                break
+            print(tok)
+
+# Build the lexer and try it out
+m = MyLexer()
+m.build()           # Build the lexer
+
+# Open file
+f = open(sys.argv[1])
+codigo = ''.join(f.readlines())
+m.test(codigo)     # Test it
+
