a4c219beb90fd964751cbe341040a7c0e0a15064
Klaus Rollmann <ra146810@yes.lab.ic.unicamp.br>
2017-05-31 18:08:20 -0300

    Continuacao do visitor

--- virtualMachine.py
+++ virtualMachine.py
@@ -2,7 +2,7 @@ class VirtualMachine(object):
   def __init__(self,code,offset, scopes, string_list):
     self.sp = 0
     self.pc = 0
-    self.M = [0]*offset
+    #self.M = [0]*offset
     self.D = [0]*scopes
     self.H = string_list
     self.label = dict()
@@ -293,8 +293,8 @@ class VirtualMachine(object):
     ("alc", n)     # Allocate memory
                       sp=sp+n
     '''
-    self.M = [0] * n
     n = inst[1]
+    self.M = [0] * n
     
     self.sp += n
     self.pc += 1
@@ -537,32 +537,3 @@ class VirtualMachine(object):
   def eval_end(self,inst):
     self.pc += 1
     pass
-    
-
-x =  [
-   ("stp",),
-   ("alc", 2),      # dcl i,k int;
-   ("rdv",),
-   ("stv", 0, 1),   # read(k);
-   ("ldc", 1),
-   ("stv", 0, 0),   # i=1;
-   ("lbl", 1),      # do
-   ("ldv", 0, 0),
-   ("ldv", 0, 1),
-   ("leq",),
-   ("jof", 2),      #   while i<=k;
-   ("ldv", 0, 0),
-   ("ldv", 0, 0),
-   ("mul",),
-   ("prv", 0),      #      print(i*i);
-   ("ldv", 0, 0),
-   ("ldc", 1),
-   ("add",),
-   ("stv", 0, 0),   #      i=i+1;
-   ("jmp", 1),      # od;
-   ("lbl", 2),
-   ("dlc", 2),
-   ("end",)
-  ]
-
-a = VirtualMachine(x,100, 10, ["Welcome.\n", "What's you name?", "Nice to meet you "])
